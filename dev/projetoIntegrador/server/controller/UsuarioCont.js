a7368642e8c00edf4123906c3306764a0c5ca273
Eduardo Bonamigo Bianchetti <185961@upf.br>
2022-09-08 21:25:12 -0300

    Início do FrontEnd com React e implementação de login com sessão e utilização de token

--- dev/projetoIntegrador/server/controller/UsuarioCont.js
+++ dev/projetoIntegrador/server/controller/UsuarioCont.js
@@ -1,5 +1,5 @@
 const Usuario = require("../model/UsuarioSchema");
-//const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt");
 
 module.exports = {
   listar: async (req, res) => {
@@ -10,16 +10,16 @@ module.exports = {
 
   incluir: async (req, res) => {
     let obj = new Usuario(req.body);
-    // const salt = await bcrypt.genSalt(Number(process.env.BCRYPT_SALT));
-    // obj.senha = await bcrypt.hash(obj.senha, salt);
+      const salt = await bcrypt.genSalt(Number(process.env.BCRYPT_SALT));
+      obj.senha = await bcrypt.hash(obj.senha, salt);
     obj.save((err, obj) => {
       err ? res.status(400).send(err) : res.status(200).json(obj);
     });
   },
   alterar: async (req, res) => {
     let obj = new Usuario(req.body);
-    // const salt = await bcrypt.genSalt(Number(process.env.BCRYPT_SALT));
-    // obj.senha = await bcrypt.hash(obj.senha, salt);
+    const salt = await bcrypt.genSalt(Number(process.env.BCRYPT_SALT));
+    obj.senha = await bcrypt.hash(obj.senha, salt);
     Usuario.updateOne({ _id: obj._id }, obj, function (err) {
       err ? res.status(400).send(err) : res.status(200).json(obj);
     });
--- dev/projetoIntegrador/server/model/UsuarioSchema.js
+++ dev/projetoIntegrador/server/model/UsuarioSchema.js
@@ -1,19 +1,19 @@
 const mongoose = require("mongoose");
-//const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const UsuarioSchema = new mongoose.Schema({
   nome: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   senha: { type: String, required: true },
-  tipo: { type: String, default: 'normal' },
+  tipo: { type: String, default: 'Normal' },
   dataNascimento: {type: Date, required: true},
   dataCadastro: {type: Date, default: Date.now}
 });
-// UsuarioSchema.methods.generateAuthToken = function () {
-//   const token = jwt.sign(
-//     { _id: this._id, nome: this.nome },
-//     process.env.JWT_PRIV_KEY,
-//     { expiresIn: process.env.TOKEN_EXPIRE }
-//   );
-//   return token;
-// };
+UsuarioSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign(
+    { _id: this._id, nome: this.nome },
+    process.env.JWT_PRIV_KEY,
+    { expiresIn: process.env.TOKEN_EXPIRE }
+  );
+  return token;
+};
 module.exports = mongoose.model("Usuario", UsuarioSchema);
